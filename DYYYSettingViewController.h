0260e15e17d472f2a606139114aa537c4d284811
Collinsquell <125298074+Collinsquell@users.noreply.github.com>
2025-03-22 22:38:05 +0800

    Update DYYYSettingViewController.h

--- DYYYSettingViewController.h
+++ DYYYSettingViewController.h
@@ -9,7 +9,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface DYYYSettingViewController : UIViewController<UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@interface DYYYSettingViewController () <UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
 @end
 
